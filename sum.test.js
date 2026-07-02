01aeac16ae82536f22d6655114613e86f0822ff0
Arjun Chavan <https://github.com/arjun28nschavan>
2026-07-02 09:18:40 +0530

    Initial touch

---
+++ sum.test.js
@@ -0,0 +1,16 @@
+const sum = require('./sum');
+
+
+describe("test for sum fnx", () =>{
+    test("adds 2 + 2 to equal 4", () => {
+        // expect().toBe(); -> expectaion , what ans should be
+        expect(sum(4, 8)).toBe(12);
+    });
+
+    test("adds -5 + -5 to equal -10", () => {
+        expect(sum(-5, -5)).toBe(-10);
+    });
+});
+
+
+//run npm test
